1317058e78ff3fff8b29890d786020fc3220a5f9
Adv1ce1 <135262373+Adv1ce1@users.noreply.github.com>
2023-08-26 17:42:26 +0300

    Update generate_unique.py (генерирует уникальные числа)

--- utils/generate_unique.py
+++ utils/generate_unique.py
@@ -3,15 +3,6 @@ import string
 
 
 def generate_unique_four_letter_value(existing_values) -> str:
-    """
-    Генерирует уникальное четырехсимвольное значение, которое не содержится в списке существующих значений.
-
-    Аргументы:
-        existing_values (list): Список существующих значений.
-
-    Возвращает:
-        str: Уникальное четырехсимвольное значение.
-    """
     alphabet = string.ascii_uppercase  # Получаем алфавит в верхнем регистре
 
     while True:
